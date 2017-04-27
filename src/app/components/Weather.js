4a99789baa745774ad1c32bdf63439720df7a769
NinoMaj <nino.majder@gmail.com>
2017-04-27 22:28:40 +0200

    depoly to heroku v10

--- src/app/components/Weather.js
+++ src/app/components/Weather.js
@@ -40,12 +40,8 @@ class Weather extends React.Component {
   }
 
   apiHelper(lat, lon) {
-    // let axiosInstance = axios.create({
-    //   baseURL: 'https://crossorigin.me/api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=f02e9ad38636be6ba2296667fbde251b',
-    //   headers: {'X-Custom-Header': 'foobar'}
-    // });
     // Move this to backend later to protect api key
-    return axios.get(`https://cors.io/?https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=f02e9ad38636be6ba2296667fbde251b`);
+    return axios.get(`https://crossorigin.me/https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=f02e9ad38636be6ba2296667fbde251b`);
   }
 
   error(err) {
